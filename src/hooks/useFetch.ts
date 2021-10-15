eaef05c897e098543dd76108cbfe27ce1a8133d1
Tanaca <jtanacar@gmail.com>
2021-10-15 20:29:44 -0300

    🏷️ type configuration SWR

--- src/hooks/useFetch.ts
+++ src/hooks/useFetch.ts
@@ -1,7 +1,7 @@
-import useSWR from "swr";
+import useSWR, { SWRConfiguration } from "swr";
 
-export function useFetch(url: string) {
-    const { data, error } = useSWR(url, async (url) => {
+export function useFetch<T = any>(url: string, options?: SWRConfiguration) {
+    const { data, error } = useSWR<T>(url, async (url) => {
         const response = await fetch(url);
         const data = await response.json();
 
