81b5b83f9a429eab9ef2b2b1818231d9fa701d24
Tanaca <jtanacar@gmail.com>
2021-10-15 20:30:13 -0300

    ♻️ remove global.css

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,6 @@
-import "../styles/globals.css";
-import "tailwindcss/tailwind.css";
 import type { AppProps } from "next/app";
 import GlobalProvider from "@atoms/GlobalProvider";
+import "tailwindcss/tailwind.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
     return (
