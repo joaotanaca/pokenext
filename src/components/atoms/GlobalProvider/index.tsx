05c6b9fda1dd27eef637c9b8e77abeb9532e1594
Tanaca <jtanacar@gmail.com>
2021-10-18 18:46:08 -0300

    ⚡️ improvement bundle framer-motion

--- src/components/atoms/GlobalProvider/index.tsx
+++ src/components/atoms/GlobalProvider/index.tsx
@@ -2,12 +2,17 @@ import React from "react";
 import { ThemeProvider } from "styled-components";
 import Global from "@styles/global";
 import theme from "@styles/theme";
+import { LazyMotion } from "framer-motion";
+
+const loadFeatures = () => import("framer-motion").then((res) => res.domMax);
 
 const GlobalProvider: React.FC = ({ children }) => (
-    <ThemeProvider theme={theme}>
-        <Global />
-        {children}
-    </ThemeProvider>
+    <LazyMotion features={loadFeatures}>
+        <ThemeProvider theme={theme}>
+            <Global />
+            {children}
+        </ThemeProvider>
+    </LazyMotion>
 );
 
 export default GlobalProvider;
